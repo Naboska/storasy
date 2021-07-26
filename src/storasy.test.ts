2c0b0d8d725cacf2de5eb31e962f7c0b23da14d0
Naboska <nabos.dezigner@gmail.com>
2021-07-26 11:52:18 +0300

    feat: add tests

--- src/storasy.test.ts
+++ src/storasy.test.ts
@@ -9,14 +9,30 @@ const INITIAL_ONE = { test: 'test' };
 
 const fake_req = () => Promise.resolve(INITIAL_ONE);
 
-function* testGenerator() {
+function* controlledGenerator() {
   yield go('loading');
   const result: TTest = yield call(fake_req, { isControl: true });
   yield put<TTest>(result);
 }
 
+function* uncontrolledGenerator() {
+  yield call(fake_req);
+}
+
 describe('client test', () => {
-  test('item created', () => {
+  test('should item create', () => {
+    let state: TTest | null = null;
+    const storasyClient = createStorasyClient();
+
+    expect(state).toBe(null);
+
+    storasyClient.create<TTest>('test', INITIAL_ONE);
+    storasyClient.subscribe<TTest>('test', item => (state = item.state));
+
+    expect(state).toBe(INITIAL_ONE);
+  });
+
+  test('should item change from put', () => {
     let state: TTest | null = null;
     const storasyClient = createStorasyClient();
 
@@ -28,14 +44,37 @@ describe('client test', () => {
     expect(state).toBe(INITIAL_ONE);
   });
 
-  test('generator work', () => {
+  test('should item change from controlled generator', async () => {
     let state: TTest | null = null;
 
     const storasyClient = createStorasyClient();
 
-    storasyClient.create('test');
-    storasyClient.subscribe<TTest>('test', item => (state = item.state));
-    storasyClient.run('test', testGenerator);
+    expect(state).toBe(null);
+
+    storasyClient.create('controlledTest');
+
+    storasyClient.subscribe<TTest>('controlledTest', item => (state = item.state));
+
+    expect(state).toBe(undefined);
+
+    storasyClient.run('controlledTest', controlledGenerator);
+
+    await new Promise(resolve => setTimeout(resolve));
+
+    expect(state).toBe(INITIAL_ONE);
+  });
+
+  test('should item change from uncontrolled generator', async () => {
+    let state: TTest | null = null;
+
+    const storasyClient = createStorasyClient();
+    storasyClient.create('uncontrolledTest');
+
+    storasyClient.subscribe<TTest>('uncontrolledTest', item => (state = item.state));
+    expect(state).toBe(undefined);
+
+    storasyClient.run('uncontrolledTest', uncontrolledGenerator);
+    await new Promise(resolve => setTimeout(resolve));
 
     expect(state).toBe(INITIAL_ONE);
   });
